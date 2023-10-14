171fe9e80d0c985dcc6717ff886330fe466e16b2
Theo BILLET <billettheo5@gmail.com>
2023-10-14 06:54:13 +0200

    game paused freeze cam

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -135,8 +135,12 @@ namespace StarterAssets
 
 		private void CameraRotation()
 		{
-			// if there is an input
-			if (_input.look.sqrMagnitude >= _threshold)
+            if (gameController.isGamePaused)
+			{
+				return;
+			}
+                // if there is an input
+                if (_input.look.sqrMagnitude >= _threshold)
 			{
 				//Don't multiply mouse input by Time.deltaTime
 				float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
